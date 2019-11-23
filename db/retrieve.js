99884ebd084486d04616147d6efcbec22494ddcc
mmayutay <raymondjay.yorong@student.passerellesnumeriques.org>
2019-11-23 17:32:38 +0800

    updated

--- db/retrieve.js
+++ db/retrieve.js
@@ -3,7 +3,7 @@ let models = require("./schema");
 module.exports = function(student, res) {
     models.Visitors.findOne({ name: student}, (err, visitors) => {
         if (err) return res.send(err)
-        return res.send(visitors.address)
+        return res.send(visitors)
     }).catch(err => {
         if (err) {
             console.log(err);
